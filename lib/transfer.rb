ba83ffc62d06b0412677832d3ccac50898d90161
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-03-14 15:32:19 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,11 +1,12 @@
 class Transfer
   
-  
+  attr_accessor :amount, :status
   
   def initialize(sender, receiver, amount, status)
     @sender = sender
     @receiver = receiver
     @amount = amount
     @status = status
+    
   
 end
